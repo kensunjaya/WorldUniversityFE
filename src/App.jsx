c64b2fa97a707a228e8618348464762f58436537
kensunjaya <kennethsunjaya@gmail.com>
2025-05-10 21:27:09 +0700

    fixed deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./components/navbar"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Search from "./pages/Search"
